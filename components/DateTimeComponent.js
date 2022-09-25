90175d11346f8c0f32ecf4cf482475a040461bda
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2022-09-24 23:06:12 -0700

    Created Date Picker with setup. Need to format

---
+++ components/DateTimeComponent.js
@@ -0,0 +1,16 @@
+// This component allows the user to be able to choose a date or a time.
+// You click on the rectangle, and it 
+
+import React, { useState, Component } from 'react';
+import DateTimePicker from "@react-native-community/datetimepicker";
+
+/**************************
+ * Props:
+ * 
+ * SetDate
+ * 
+ */
+
+export default class DateTimeComponent extends Component {
+
+}
