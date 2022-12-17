a700a34ce54f875efc6938c42c67d0c4156a1bea
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2022-12-16 16:17:34 -0800

    Rename notifications

--- screens/Authentication/Signup.js
+++ screens/Authentication/Signup.js
@@ -25,7 +25,7 @@ import { Dimensions } from "react-native";
 import SelectList from 'react-native-dropdown-select-list';
 import { AuthContext } from '../../AuthContext';
 import UsedServer from "../../constants/servercontants";
-import registerForPushNotificationsAsync from "../../Services/NotificationsService";
+import registerForPushNotificationsAsync from "../../Services/NotificationService";
 
 var width = Dimensions.get('window').width; //full width
 var height = Dimensions.get('window').height; //full height
