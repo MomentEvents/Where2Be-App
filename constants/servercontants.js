84252a7ec3449eca06cf472117e4f68090088475
Chirag Rastogi <chiragr2@illinois.edu>
2023-01-02 11:42:16 -0600

    server constants changed

--- constants/servercontants.js
+++ constants/servercontants.js
@@ -1,7 +1,7 @@
-const testserver = 'http://54.214.99.208:8080'
-const localhost = 'localhost:8080'
-const androidhost = 'http://10.0.2.2:8080'
+const testserver = "http://54.201.227.203:8080";
+const localhost = "localhost:8080";
+const androidhost = "http://10.0.2.2:8080";
 
-const UsedServer = testserver
+const UsedServer = testserver;
 
 export default UsedServer;
