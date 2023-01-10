5c070220cfc91b82a9aec8849c742333919e8eac
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-01-10 09:53:11 -0800

    TODO: bugfix signup and login input texts scrolling

--- src/screens/unauthorized/Signup/SignupScreen.tsx
+++ src/screens/unauthorized/Signup/SignupScreen.tsx
@@ -1,112 +1,140 @@
 import {
-    Button,
-    KeyboardAvoidingView,
-    SafeAreaView,
-    ScrollView,
-    StyleSheet,
-    Text,
-    View,
-  } from "react-native";
-  import React, { useContext, useState } from "react";
-  import { UserContext } from "../../../contexts/UserContext";
-  import { AuthContext } from "../../../contexts/AuthContext";
-  import GradientBackground from "../../../components/Styled/GradientBackground";
-  import { McText } from "../../../components/Styled";
-  import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
-  import { COLORS, FONTS, SIZES } from "../../../constants";
-  import { CUSTOMFONT_REGULAR } from "../../../constants/theme";
-  import { ScreenContext } from "../../../contexts/ScreenContext";
-  import { displayError } from "../../../helpers/helpers";
-  
-  const SignupScreen = () => {
-    const { userLogin } = useContext(AuthContext);
-    const { setLoading } = useContext(ScreenContext);
-  
-    const [usercred, setUsercred] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-  
-    const onLogin = () => {
-      setLoading(true);
-      userLogin(usercred, password)
-        .then(() => setLoading(false))
-        .catch((error) => {
-          displayError(error);
-          setLoading(false);
-        });
-    };
-    return (
-      <GradientBackground>
-        <ScrollView
-          contentContainerStyle={{ flexGrow: 1 }}
-          keyboardShouldPersistTaps="handled"
-        >
-          <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
-            <SafeAreaView style={styles.container}>
-              <View>
-                <McText h1>Welcome to Moment!</McText>
-              </View>
-              <TextInput
-                placeholder={"username / email"}
-                placeholderTextColor={COLORS.gray}
-                style={styles.textInputContainer}
-                onChangeText={(newText) => setUsercred(newText)}
-              />
-              <TextInput
-                placeholder={"password"}
-                placeholderTextColor={COLORS.gray}
-                style={styles.textInputContainer}
-                onChangeText={(newText) => setPassword(newText)}
-                secureTextEntry={true}
-              />
-              <TouchableOpacity onPress={onLogin}>
-                <View style={styles.submitButton}>
-                  <McText
-                    h4
-                    style={{
-                      color: COLORS.black,
-                    }}
-                  >
-                    Login
-                  </McText>
-                </View>
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <McText b3>Don't have an account? Create one here!</McText>
-              </TouchableOpacity>
-            </SafeAreaView>
-          </KeyboardAvoidingView>
-        </ScrollView>
-      </GradientBackground>
-    );
+  Button,
+  KeyboardAvoidingView,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React, { useContext, useState } from "react";
+import { UserContext } from "../../../contexts/UserContext";
+import { AuthContext } from "../../../contexts/AuthContext";
+import GradientBackground from "../../../components/Styled/GradientBackground";
+import { McText } from "../../../components/Styled";
+import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
+import { COLORS, FONTS, SCREENS, SIZES, School } from "../../../constants";
+import { CUSTOMFONT_REGULAR } from "../../../constants/theme";
+import { ScreenContext } from "../../../contexts/ScreenContext";
+import { displayError, formatError } from "../../../helpers/helpers";
+import * as Navigator from "../../../navigation/Navigator";
+import SchoolSelector from "./components/SchoolSelector";
+
+const SignupScreen = () => {
+  const { userSignup } = useContext(AuthContext);
+  const { setLoading } = useContext(ScreenContext);
+
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [displayName, setDisplayName] = useState<string>("");
+  const [selectedSchool, setSelectedSchool] = useState<School>(null);
+  const [password, setPassword] = useState<string>("");
+
+  const onSignup = () => {
+    if(selectedSchool === null){
+        displayError(formatError("Input error", "Please select a valid school"))
+        return;
+    }
+    setLoading(true);
+    userSignup(username, displayName, email, password, selectedSchool.SchoolID)
+      .then(() => setLoading(false))
+      .catch((error) => {
+        displayError(error);
+        setLoading(false);
+      });
   };
-  
-  export default SignupScreen;
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    textInputContainer: {
-      width: SIZES.width / 1.3,
-      borderColor: COLORS.white,
-      borderWidth: 1,
-      borderRadius: 5,
-      paddingHorizontal: 10,
-      fontFamily: CUSTOMFONT_REGULAR,
-      fontSize: 16,
-      color: COLORS.white,
-      paddingVertical: 10,
-      marginTop: 20,
-    },
-    submitButton: {
-      borderRadius: 5,
-      paddingHorizontal: 20,
-      paddingVertical: 10,
-      backgroundColor: COLORS.white,
-      marginTop: 30,
-      marginBottom: 60,
-    },
-  });
-  
+
+  const onNavigateLogin = () => {
+    Navigator.navigate(SCREENS.Login);
+  };
+
+  return (
+    <GradientBackground>
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+          <SafeAreaView style={styles.container}>
+            <View>
+              <McText h1>Welcome to Moment!</McText>
+            </View>
+            <TextInput
+              placeholder={"name"}
+              placeholderTextColor={COLORS.gray}
+              style={styles.textInputContainer}
+              onChangeText={(newText) => setDisplayName(newText)}
+            />
+            <TextInput
+              placeholder={"username"}
+              placeholderTextColor={COLORS.gray}
+              style={styles.textInputContainer}
+              onChangeText={(newText) => setUsername(newText)}
+            />
+            <TextInput
+              placeholder={"email"}
+              placeholderTextColor={COLORS.gray}
+              style={styles.textInputContainer}
+              onChangeText={(newText) => setEmail(newText)}
+            />
+            <TextInput
+              placeholder={"password"}
+              placeholderTextColor={COLORS.gray}
+              style={styles.textInputContainer}
+              onChangeText={(newText) => setPassword(newText)}
+              secureTextEntry={true}
+            />
+            <View style={styles.textInputContainer} >
+              <SchoolSelector setSelectedSchool={setSelectedSchool}/>
+            </View>
+            <TouchableOpacity onPress={onSignup}>
+              <View style={styles.submitButton}>
+                <McText
+                  h4
+                  style={{
+                    color: COLORS.black,
+                  }}
+                >
+                  Signup
+                </McText>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={onNavigateLogin}>
+              <McText b3>Already have an account? Login here!</McText>
+            </TouchableOpacity>
+          </SafeAreaView>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </GradientBackground>
+  );
+};
+
+export default SignupScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textInputContainer: {
+    width: SIZES.width / 1.3,
+    borderColor: COLORS.white,
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    fontFamily: CUSTOMFONT_REGULAR,
+    fontSize: 16,
+    color: COLORS.white,
+    paddingVertical: 10,
+    marginTop: 20,
+  },
+  submitButton: {
+    borderRadius: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: COLORS.white,
+    marginTop: 30,
+    marginBottom: 60,
+  },
+});
