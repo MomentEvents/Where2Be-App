1526794440cacd108d32732d3722dc01300c8118
Chirag5241 <crastogi2000@gmail.com>
2023-02-21 03:42:57 -0600

    https server change

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,6 +1,5 @@
-const productionserver = "http://3.136.67.161:8080";
+const productionserver = "https://api.momentevents.app";
 const version = "/api_ver_1.0.0";
-
 
 export const momentAPIVersionless = productionserver;
 export const momentAPI = momentAPIVersionless + version;
