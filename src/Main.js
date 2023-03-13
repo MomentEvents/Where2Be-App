013df1e4835b589ab3848c305833703a5f4d3a87
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-03-12 22:44:00 -0700

    Started background restart

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,67 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  AppState,
+  StatusBar,
+} from "react-native";
 import AppNav from "./navigation/AppNav";
 import { UserProvider } from "./contexts/UserContext";
 import { ScreenProvider } from "./contexts/ScreenContext";
 import { AuthProvider } from "./contexts/AuthContext";
 import { EventProvider } from "./contexts/EventContext";
-import { StatusBar } from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Updates from "expo-updates";
+import AppUpdates from "expo-splash-screen";
 
 const Main = () => {
+  const [appState, setAppState] = useState(AppState.currentState);
+  var isBackground = false;
+  var lastActiveTime = Date.now();
+
+  useEffect(() => {
+    console.log("in initial use effect");
+    const handleAppStateChange = (nextAppState) => {
+      console.log("---------------------------------");
+      console.log("Checking phone timeout");
+      console.log("Current app state is " + appState);
+      console.log("nextAppState is " + nextAppState);
+      console.log(
+        "Difference between now and last active time is " +
+          (Date.now() - lastActiveTime)
+      );
+
+      console.log("isBackground is " + isBackground);
+      if (nextAppState === "active") {
+        console.log("setting last active time");
+        lastActiveTime = Date.now();
+      }
+      if (
+        isBackground &&
+        nextAppState === "active" &&
+        Date.now() - lastActiveTime >= 5000
+      ) {
+        console.log("restarting app");
+        Updates.reloadAsync();
+      }
+
+      if (nextAppState === "background") {
+        isBackground = true;
+      } else {
+        isBackground = false;
+      }
+      console.log("---------------------------------");
+    };
+    const subscription = AppState.addEventListener(
+      "change",
+      handleAppStateChange
+    );
+    return () => {
+      subscription.remove();
+    };
+  });
+
   // Disable font scaling
   Text.defaultProps = Text.defaultProps || {};
   Text.defaultProps.allowFontScaling = false;
