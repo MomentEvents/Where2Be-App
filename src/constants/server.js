8a846ee2ba9f002a4331fee71ab6a0a1d4f6a355
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-04-07 16:37:58 -0700

    Update server.js

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,5 +1,9 @@
-const productionserver = "https://api.momentevents.app";
+// PUT YOUR LOCALHOST IP ADDRESS HERE WHERE THE API CONTAINER IS RUNNING
+const localhost = "http://100.91.66.118:8080";
+const production = "https://api.momentevents.app";
+
+const server = __DEV__ ? localhost : production
 const version = "/api_ver_1.0.0";
 
-export const momentAPIVersionless = productionserver;
+export const momentAPIVersionless = server;
 export const momentAPI = momentAPIVersionless + version;
