42e6a027179bc49f82120cd9e1ae2d611cd7be30
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-07-10 04:11:00 -0700

    Added EAS update

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Alert, StyleSheet, Text, TextInput, View } from "react-native";
 import AppNav from "./navigation/AppNav";
 import { UserProvider } from "./contexts/UserContext";
 import { ScreenProvider } from "./contexts/ScreenContext";
@@ -7,6 +7,8 @@ import { EventProvider } from "./contexts/EventContext";
 import { StatusBar } from "react-native";
 import * as Notifications from 'expo-notifications';
 import { useEffect } from "react";
+import * as Updates from 'expo-updates';
+
 
 // Set the handler that's invoked whenever a notification is received when the app is open
 Notifications.setNotificationHandler({
@@ -17,37 +19,25 @@ Notifications.setNotificationHandler({
   }),
 });
 
+
+
 const Main = () => {
+
   useEffect(() => {
-    // This listener is fired whenever a notification is received while the app is foregrounded
-    const foregroundSubscription = Notifications.addNotificationReceivedListener(notification => {
-      console.log('Notification received in foreground:', notification);
-    });
-
-    // This listener is fired whenever a user taps on or interacts with a notification
-    const responseSubscription = Notifications.addNotificationResponseReceivedListener(response => {
-      const { notification } = response;
-      console.log('User interacted with this notification:', notification);
-
-      // The notification's data is where you put your own custom payload
-      const { data } = notification.request.content;
-
-      if (data.myCommand === 'openDialog') {
-        // Here you perform your command based on the payload
-        console.log('Opening dialog...');
-      } else if (data.myCommand === 'navigateToScreen') {
-        // Or navigate to a specific screen, etc.
-        console.log('Navigating to screen...');
-      }
-    });
-
-    return () => {
-      // Clean up on unmount
-      Notifications.removeNotificationSubscription(foregroundSubscription);
-      Notifications.removeNotificationSubscription(responseSubscription);
-    };
-  }, []);
+    reactToUpdates();
+  })
   
+  const reactToUpdates = () => {
+    Updates.addListener((event) => {
+      if(event.type === Updates.UpdateEventType.ERROR){
+        console.log("UPDATE ERROR: COULD NOT FETCH LATEST EXPO UPDATE")
+      }
+      if (event.type === Updates.UpdateEventType.UPDATE_AVAILABLE)
+        Alert.alert("New update available", "Restarting the app...")
+        Updates.reloadAsync();
+    })
+  }
+
   return (
     <UserProvider>
       <EventProvider>
