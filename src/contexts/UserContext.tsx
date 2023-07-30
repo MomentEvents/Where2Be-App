f249e9f003d5f86c44cbd0a958739f025a7380c0
Kyle Wade <59634395+kyle1373@users.noreply.github.com>
2023-07-30 02:23:35 -0700

    Added follow rate limiting

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -9,7 +9,14 @@
  * AuthContext.
  */
 
-import React, { useState, useEffect, createContext, useReducer, useContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  createContext,
+  useReducer,
+  useContext,
+  useRef,
+} from "react";
 import { User, School, Token } from "../constants";
 import {
   checkIfUserAccessTokenIsAdmin,
@@ -36,11 +43,10 @@ import {
 } from "../services/NotificationService";
 import { CustomError } from "../constants/error";
 import { AlertContext } from "./AlertContext";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "../redux/store";
-import { selectUserByID } from '../redux/users/userSelectors';
+import { selectUserByID } from "../redux/users/userSelectors";
 import { setUserMap, updateUserFollowUser } from "../redux/users/userSlice";
-
 
 type UserContextType = {
   userToken: Token;
@@ -73,7 +79,7 @@ export const UserContext = createContext<UserContextType>({
 
 export const UserProvider = ({ children }) => {
   const [userToken, setUserToken] = useState<Token>(null);
-  
+
   const [currentSchool, setCurrentSchool] = useState<School>(null);
   const [isUserContextLoaded, setIsUserContextLoaded] =
     useState<boolean>(false);
@@ -83,37 +89,109 @@ export const UserProvider = ({ children }) => {
 
   const dispatch = useDispatch<AppDispatch>();
 
-  const {showErrorAlert} = useContext(AlertContext)
+  const { showErrorAlert } = useContext(AlertContext);
 
+  const lastFollowedActionEventIDToTime = useRef<{ [key: string]: Date }>({});
+  const lastUnfollowedActionEventIDToTime = useRef<{ [key: string]: Date }>({});
+  const rateLimitInSeconds = 3;
+
+  const checkLastAction = (
+    type: "follow" | "unfollow",
+    userID: string
+  ): boolean => {
+    const rightNow = new Date();
+    let checkDate: Date;
+
+    if (type === "follow") {
+      checkDate = lastFollowedActionEventIDToTime.current[userID];
+    } else {
+      checkDate = lastUnfollowedActionEventIDToTime.current[userID];
+    }
+
+    if (!checkDate) {
+      // action performed for the first time
+      if (type === "follow") {
+        lastFollowedActionEventIDToTime.current[userID] = rightNow;
+      } else {
+        lastUnfollowedActionEventIDToTime.current[userID] = rightNow;
+      }
+      return true;
+    }
+
+    console.log(rightNow.getTime());
+    console.log(checkDate.getTime());
+
+    console.log(rightNow.getTime() - checkDate.getTime());
+    console.log(rateLimitInSeconds * 1000);
+
+    if (rightNow.getTime() - checkDate.getTime() > rateLimitInSeconds * 1000) {
+      if (type === "follow") {
+        lastFollowedActionEventIDToTime.current[userID] = rightNow;
+      } else {
+        lastUnfollowedActionEventIDToTime.current[userID] = rightNow;
+      }
+      return true;
+    }
+
+    return false;
+  };
 
   const clientUnfollowUser = async (userID: string): Promise<void> => {
-    console.log("Follow user hit");
-
-    dispatch(updateUserFollowUser({ fromID: userToken.UserID, toID: userID, doFollow: false }));
+    console.log("Unfollow user hit");
+    if (!checkLastAction("unfollow", userID)) {
+      return;
+    }
+    dispatch(
+      updateUserFollowUser({
+        fromID: userToken.UserID,
+        toID: userID,
+        doFollow: false,
+      })
+    );
 
     unfollowUser(userToken.UserAccessToken, userToken.UserID, userID).catch(
       (error: CustomError) => {
         if (error.showBugReportDialog) {
           showBugReportPopup(error);
         }
-        showErrorAlert(error)
-        dispatch(updateUserFollowUser({ fromID: userToken.UserID, toID: userID, doFollow: true }));
+        showErrorAlert(error);
+        dispatch(
+          updateUserFollowUser({
+            fromID: userToken.UserID,
+            toID: userID,
+            doFollow: true,
+          })
+        );
       }
     );
   };
 
   const clientFollowUser = async (userID: string): Promise<void> => {
-    console.log("Unfollow user hit");
-
-    dispatch(updateUserFollowUser({ fromID: userToken.UserID, toID: userID, doFollow: true }));
+    console.log("Follow user hit");
+    if (!checkLastAction("follow", userID)) {
+      return;
+    }
+    dispatch(
+      updateUserFollowUser({
+        fromID: userToken.UserID,
+        toID: userID,
+        doFollow: true,
+      })
+    );
 
     followUser(userToken.UserAccessToken, userToken.UserID, userID).catch(
       (error: CustomError) => {
         if (error.showBugReportDialog) {
           showBugReportPopup(error);
         }
-        showErrorAlert(error)
-        dispatch(updateUserFollowUser({ fromID: userToken.UserID, toID: userID, doFollow: false }));
+        showErrorAlert(error);
+        dispatch(
+          updateUserFollowUser({
+            fromID: userToken.UserID,
+            toID: userID,
+            doFollow: false,
+          })
+        );
       }
     );
   };
@@ -131,7 +209,7 @@ export const UserProvider = ({ children }) => {
       if (error.showBugReportDialog) {
         showBugReportPopup(error);
       } else {
-        showErrorAlert(error)
+        showErrorAlert(error);
       }
     });
     updateFirstInstall(false);
