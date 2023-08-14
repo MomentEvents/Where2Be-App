1910667513849e24c53e779aecad7d818d0a3964
Kyle Wade <59634395+kyle1373@users.noreply.github.com>
2023-08-13 21:57:38 -0700

    Changed linking API and universal links function

--- ios/Where2Be/AppDelegate.mm
+++ ios/Where2Be/AppDelegate.mm
@@ -9,6 +9,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  
   [FIRApp configure];
   [RNBranch initSessionWithLaunchOptions:launchOptions isReferrable:YES];
   NSURL *jsCodeLocation;
@@ -17,6 +18,13 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
+    // Listener for Branch deep link data
+  [[Branch getInstance] initSessionWithLaunchOptions:launchOptions andRegisterDeepLinkHandler:^(NSDictionary * _Nonnull params, NSError * _Nullable error) {
+		NSLog(@"%@", params);
+		// Access and use deep link data here (nav to page, display content, etc.)
+    NSLog(@"APPDELEGATE DEEP LINK WAS DETECTED");
+  }];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
@@ -31,14 +39,25 @@
 }
 
 // Linking API
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-  return [super application:application openURL:url options:options] || [RCTLinkingManager application:application openURL:url options:options] || [RNBranch application:application openURL:url options:options];
+// - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+//   return [super application:application openURL:url options:options] || [RCTLinkingManager application:application openURL:url options:options] || [RNBranch application:application openURL:url options:options];
+// }
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+  [[Branch getInstance] application:app openURL:url options:options];
+  return YES;
 }
 
-// Universal Links
-- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
-  BOOL result = [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
-  return [super application:application continueUserActivity:userActivity restorationHandler:restorationHandler] || result || [RNBranch continueUserActivity:userActivity];
+// // Universal Links
+// - (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
+//   BOOL result = [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
+//   return [super application:application continueUserActivity:userActivity restorationHandler:restorationHandler] || result || [RNBranch continueUserActivity:userActivity];
+// }
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray * _Nullable))restorationHandler {
+  // Handler for Universal Links
+  [[Branch getInstance] continueUserActivity:userActivity];
+  return YES;
 }
 
 // Explicitly define remote notification delegates to ensure compatibility with some third-party libraries
@@ -54,9 +73,15 @@
 }
 
 // Explicitly define remote notification delegates to ensure compatibility with some third-party libraries
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
-{
-  return [super application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+// - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+// {
+//   [[Branch getInstance] handlePushNotification:userInfo];
+//   return [super application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+// }
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+  // Handler for Push Notifications
+  [[Branch getInstance] handlePushNotification:userInfo];
 }
 
 @end
