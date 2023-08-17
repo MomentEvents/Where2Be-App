0e0f4dc745f74f3084e0636066f4ddd054f64661
Kyle Wade <59634395+kyle1373@users.noreply.github.com>
2023-08-17 12:02:09 -0700

    Added android build

--- App.tsx
+++ App.tsx
@@ -4,16 +4,16 @@ import * as Sentry from 'sentry-expo';
 import { SETTINGS } from "./src/constants/settings";
 import { View } from "react-native";
 
-// Sentry.init({
-//   dsn: 'https://4ec821edf22c4d9fb0a9241f1d40480d@o4505576552333312.ingest.sentry.io/4505580927123456',
-//   enableInExpoDevelopment: SETTINGS.sentryEnabled,
-//   debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
-// });
+Sentry.init({
+  dsn: 'https://4ec821edf22c4d9fb0a9241f1d40480d@o4505576552333312.ingest.sentry.io/4505580927123456',
+  enableInExpoDevelopment: SETTINGS.sentryEnabled,
+  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
 
 export default function App() {
   
   console.log("Where2Be App is running!")
   return (
-    <View/>
+    <Main/>
   );
 }
